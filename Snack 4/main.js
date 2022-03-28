e680a02f077f181752e5f9f8432cae027e6d7dc4
catclou <caterina.moccia@gmail.com>
2022-03-28 17:53:14 +0200

    Snack 4 COMPLETO (3 milestone)

--- Snack 4/main.js
+++ Snack 4/main.js
@@ -14,6 +14,7 @@ let students = [
     { name: 'Francesca', id: 120, grades: 84 },
 ];
 
+// MILESTONE 1:
 let targheMaiuscolo = students.map( // uso map per ciclare su tutto l'array students e salvare i risultati nella variabile 'targheMaiuscolo'
     (element) => {
         return element.name.toUpperCase(); // ad ogni stringa associata alla proprietà 'name' attribuisco il metodo toUpperCase per trasformarla in maiuscolo e riversarla nella variabile 'targheMaiuscolo'
@@ -21,3 +22,11 @@ let targheMaiuscolo = students.map( // uso map per ciclare su tutto l'array stud
 )
 
 console.log(targheMaiuscolo); // Ottengo un array con tutti i nomi degli studenti in maiuscolo
+
+// MILESTONE 2:
+const votoSuperioreA70 = students.filter((element) => element.grades > 70); // Raccolgo in 'votoSuperioreA70' solo gli studenti che hanno un valore superiore a 70 associato alla proprietà 'grades'
+console.log(votoSuperioreA70); // Ottengo Marco, Paola, Gaia e Francesca
+
+// MILESTONE 3:
+const superStudenti = students.filter((element) => element.grades > 70 && element.id > 120); // Come sopra, ma aggiungo con AND la condizione in cui l'id è superiore a 120
+console.log(superStudenti); // Ottengo Marco e Gaia
